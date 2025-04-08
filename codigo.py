1fb99b07bf99ef2eca04ef7b4a3ab47341024bcd
Daniel Pantoja <danieljuniorpantojapureza@gmail.com>
2025-04-08 09:55:57 -0300

    adicionar lógica do Projeto

---
+++ codigo.py
@@ -0,0 +1,26 @@
+import pyautogui
+
+# pyautogui.click --> clicar em algum lugar
+# pyautogui.press --> apertar 1 tecla
+# pyautogui.write --> escrever um texto
+# pyautogui.hotkey --> apertar uma combinação de teclas 
+
+# Lógica: 
+
+# abrir o navegador 
+# entrar no navegador
+# digitar o link e entrar no site
+
+# fazer login 
+# selecionar o campo de email
+# escrever email
+# preencher senha
+
+# importar a base de produtos pra cadastrar
+
+# cadastrar um produto
+# preencher campos
+
+# dar scroll de tudo pra cima
+
+# repetir o processo até o fim.
